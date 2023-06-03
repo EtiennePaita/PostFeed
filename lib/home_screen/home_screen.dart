40d4d54aca7c0d6c7abf69ecd61358b47d4bcb71
Etienne Païta <etienne@studio-equinoxe.com>
2023-06-03 17:50:27 +0200

    [HomeScreen] Add PostItem

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:post_feed/home_screen/models/post.dart';
+import 'package:post_feed/home_screen/post_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +12,15 @@ class HomeScreen extends StatelessWidget {
         title: const Text('PostFeed'),
       ),
       body: Column(
-        children: const [
-          Text('data')
+        children: [
+          PostItem(
+            post: Post(
+              id: 'SUUU',
+              title: 'My first post !',
+              description: '#firstTime #ecxiting #blog',
+              content: 'This is my first post ever ! I will post regularly contents about me, about my dog and about my golden fish Charlysh. Stay awake ! :)',
+            ),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
@@ -26,5 +34,4 @@ class HomeScreen extends StatelessWidget {
     debugPrint("_onAddPostClick");
     //navigate to add post form
   }
-
 }
---
+++ lib/home_screen/models/post.dart
@@ -0,0 +1,13 @@
+class Post {
+  final String id;
+  final String title;
+  final String description;
+  final String content;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.content,
+  });
+}
